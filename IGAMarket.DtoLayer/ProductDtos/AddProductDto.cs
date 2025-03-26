dda562940321eeb79b3353efee0d314027d296c5
beratmusdal <beratmsdl@gmail.clm>
2025-03-26 09:16:34 +0300

    commit

--- IGAMarket.DtoLayer/ProductDtos/AddProductDto.cs
+++ IGAMarket.DtoLayer/ProductDtos/AddProductDto.cs
@@ -1,4 +1,6 @@
-﻿namespace IGAMarket.DtoLayer.ProductDtos;
+﻿using IGAMarket.EntityLayer.Enums;
+
+namespace IGAMarket.DtoLayer.ProductDtos;
 
 public class AddProductDto
 {
@@ -6,6 +8,7 @@ public class AddProductDto
     public string Barcode { get; set; }
     public decimal PurchasePrice { get; set; } // Alış fiyatı
     public decimal SalePrice { get; set; } // Satış fiyatı
+    public CategoryEnum Category { get; set; }
     public int StockQuantity { get; set; } // Stok miktarı
     public string ImageUrl { get; set; } // Ürün fotoğrafı
 }
--- IGAMarket.DtoLayer/ProductDtos/ResultProductDto.cs
+++ IGAMarket.DtoLayer/ProductDtos/ResultProductDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IGAMarket.EntityLayer.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +12,10 @@ namespace IGAMarket.DtoLayer.ProductDtos
         public long Id { get; set; }
         public string Name { get; set; }
         public long Barcode { get; set; }
+        public CategoryEnum Category { get; set; }
         public decimal PurchasePrice { get; set; } // Alış fiyatı
         public decimal SalePrice { get; set; } // Satış fiyatı
         public int StockQuantity { get; set; } // Stok miktarı
-        public string Category { get; set; }
         public string? ImageUrl { get; set; } // Ürün fotoğrafı
         public DateTime CreateDate { get; set; }
     }   
